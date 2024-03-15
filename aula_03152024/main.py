dc13d4cec01486a972653da277ff3d2647f96694
DougzCD <magonegrodcd@gmail.com>
2024-03-15 09:42:10 -0300

    2 parte

--- aula_03152024/main.py
+++ aula_03152024/main.py
@@ -1,7 +1,15 @@
 from student import Student
 
 nameStudent = input("Student name: ")
+rollStudent = input("Student roll: ")
+ageStudent = input("Student age: ")
 qttGrades = int(input("Number of grades: "))
-myStudent = Student(nameStudent, qttGrades)
+myStudent = Student(nameStudent, rollStudent, ageStudent, qttGrades)
 
-print(myStudent.__str__)
+print(myStudent.__str__())
+print("\nTesting")
+print(myStudent.name)
+print(myStudent.getRoll())
+print(myStudent._roll)
+print(myStudent.getAge())
+print(myStudent.__age)
--- aula_03152024/student.py
+++ aula_03152024/student.py
@@ -1,15 +1,23 @@
 class Student:
 
-    def __init__(self, name, number):
+    def __init__(self, name, roll, age, number):
         self.name = name
+        self._roll = roll
+        self.__age = age
         self.scores = []
         for count in range(number):
             self.scores.append(0)
             
     def __str__(self) -> str:
-        return "Name:" + self.name + "/nScores:" + \
+        return "Name:" + self.name + "\nRoll:" + self._roll +\
+            "\nage:" + self.__age + "\nScores:" + \
             " " .join(map(str, self.scores))
             
     def getName(self):
         return self.name
-        
+    
+    def getRoll(self):
+        return self._roll
+    
+    def getAge(self):
+        return self.__age
